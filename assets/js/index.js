a407b233176ff7d796528150b564a88f011d9ca5
abdulogs <abdulogs88@gmail.com>
2024-03-14 12:16:23 +0500

    fixes

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,14 +6,33 @@
 //   }
 // });
 
+// document.addEventListener("click", function (e) {
+//   const target = e.target;
+//   if (target.matches('[data-nxt-toggle="collapse"]')) {
+//     let collapseItem = target.parentElement;
+//     let collapsible = collapseItem.parentElement;
+//     let items = collapsible.querySelectorAll(".collapse-item");
+
+//     items.forEach((el) => el.classList.remove("active"));
+//     collapseItem.classList.toggle("active");
+//   }
+// });
+
+
+
 document.addEventListener("click", function (e) {
   const target = e.target;
-  if (target.matches('[data-nxt-toggle="collapse"]')) {
-    let collapseItem = target.parentElement;
-    let collapsible = collapseItem.parentElement;
-    let items = collapsible.querySelectorAll(".collapse-item");
+  if (target.matches('[data-nxt-toggle="tab"]')) {
+    const active =  target;
+    const parent = target.parentElement.parentElement
+    const btns = parent.querySelectorAll(".tab-item")
+    const panes = parent.querySelectorAll(".tab-pane")
 
-    items.forEach((el) => el.classList.remove("active"));
-    collapseItem.classList.toggle("active");
+    panes.forEach(el => el.classList.remove("active"));
+    btns.forEach(el => el.classList.remove("active"));
+
+    active.classList.add("active")
+    const attr = active.getAttribute("data-nxt-target")
+    document.querySelector(attr).classList.add("active")
   }
 });
